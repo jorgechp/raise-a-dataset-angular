7cc7311be1aaaffab5bc1c83ef7d7b59ea8240bb
jorgechp <jorge.chamorro@udl.cat>
2024-08-20 00:45:54 +0200

    Add change password functionality

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,6 +1,6 @@
 import {Component, inject, OnInit} from '@angular/core';
 
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators,} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators,} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
@@ -15,7 +15,7 @@ import {TranslocoDirective} from "@jsverse/transloco";
 import {AuthenticationService} from "../../services/authentication/authentication.service";
 import {NavigationEnd, Router} from "@angular/router";
 import {getIdFromURI} from "../utils/funcions";
-import {MatSlideToggle, MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatSlideToggleChange, MatSlideToggleModule} from "@angular/material/slide-toggle";
 import {MatFormFieldModule} from "@angular/material/form-field";
 
 
@@ -46,8 +46,25 @@ export class RegisterFormComponent implements OnInit{
   protected isUserCreated = false;
   protected isUserSettings = false;
   protected isChangePassword = false;
+  protected form?: FormGroup;
   private currentUser?: User;
+  private fb = inject(FormBuilder);
 
+  private usernameFormControl = new FormControl('', Validators.required);
+  private emailFormControl = new FormControl('', Validators.compose([Validators.required, Validators.email]));
+  private currentPasswordFormControl = new FormControl('', Validators.required);
+  private password1FormControl = new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.pattern(StrongPasswordRegx),
+      Validators.minLength(8),
+      Validators.maxLength(50)
+    ]
+  ));
+  private password2FormControl = new FormControl(null, Validators.compose([
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(50)
+  ]));
 
   constructor(private userService: UserService,
               private authenticationService: AuthenticationService,
@@ -59,70 +76,44 @@ export class RegisterFormComponent implements OnInit{
     });
   }
 
-  private fb = inject(FormBuilder);
-  signupForm = this.fb.group({
-    username: ['', Validators.required],
-    email: ['', Validators.compose([Validators.required, Validators.email])],
-    ageConditions: [, Validators.requiredTrue],
-    acceptedConditions: [, Validators.requiredTrue],
-    password1: ['', Validators.compose([
-      Validators.required,
-      Validators.pattern(StrongPasswordRegx),
-      Validators.minLength(8),
-      Validators.maxLength(50)
-      ]
-    )
-    ],
-    password2: [null, Validators.compose([
-      Validators.required,
-      Validators.minLength(8),
-      Validators.maxLength(50)
-    ])
-    ]
-  }, {
-    validators: [PasswordMatchValidator.match('password1', 'password2')]
-  });
-
-  updateSettingsForm = this.fb.group({
-    username: ['', Validators.required],
-    email: ['', Validators.compose([Validators.required, Validators.email])]
-
-  }, {});
-
-  protected getFormControl() {
-    if(this.isUserSettings){
-      return this.updateSettingsForm;
-    }
-    return this.signupForm;
-  }
-
-
   ngOnInit(): void {
     if(this.isUserSettings){
+      this.form = this.getUpdateSettingsForm();
       this.userService.getResource(Number(getIdFromURI(this.authenticationService.getCurrentUser().uri!))).subscribe(
           (response: User) => {
             this.currentUser = response;
-            this.currentUser.username && this.updateSettingsForm.get('username')!.setValue(this.currentUser.username);
-            this.currentUser.email && this.updateSettingsForm.get('email')!.setValue(this.currentUser.email);
+            this.currentUser.username && this.form!.get('username')!.setValue(this.currentUser.username);
+            this.currentUser.email && this.form!.get('email')!.setValue(this.currentUser.email);
           }
       );
+    } else {
+      this.form = this.getSignUpForm();
     }
   }
 
   onSubmit(): void {
-    if(this.signupForm.valid){
-      const user = new User();
-      user.username = this.signupForm.get("username")!.value!;
-      user.email = this.signupForm.get("email")!.value!;
-      user.password = this.signupForm.get("password1")!.value!;
-      this.userService.add(user).subscribe(value => {
-        this.isUserCreated = true;
-      });
+    if (this.form && this.form.valid) {
+      const user = this.isUserSettings && this.currentUser ? this.currentUser : new User();
+      user.username = this.form.get("username")!.value!;
+      user.email = this.form.get("email")!.value!;
+      user.password = this.form.get("password1")!.value!;
+      if (this.isUserSettings) {
+        const currentPassword = this.form.get("currentPassword")!.value!;
+        this.userService.changePassword(currentPassword, user.password!).subscribe(() => {
+          this.userService.add(user).subscribe(value => {
+            this.isUserCreated = true;
+          });
+        });
+      } else {
+        this.userService.updateResource(user).subscribe(value => {
+          this.isUserCreated = true;
+        });
+      }
     }
   }
 
   getErrorInPassword(field: string): PASSWORD_ERROR_TYPE[] {
-    const passwordFormField = this.signupForm.get(field);
+    const passwordFormField = this.form!.get(field);
     const value = (passwordFormField && passwordFormField.value)?passwordFormField.value:'';
 
     let errorType: PASSWORD_ERROR_TYPE[] = [];
@@ -140,5 +131,40 @@ export class RegisterFormComponent implements OnInit{
       errorType.push(PASSWORD_ERROR_TYPE.SPECIAL_CHARACTER);
     }
     return errorType;
+  }
+
+  doChangePassword($event: MatSlideToggleChange) {
+    if ($event.checked) {
+      this.form?.addControl('currentPassword', this.currentPasswordFormControl);
+      this.form?.addControl('password1', this.password1FormControl);
+      this.form?.addControl('password2', this.password2FormControl);
+      this.form?.addValidators([PasswordMatchValidator.match('password1', 'password2')]);
+    } else {
+      this.form?.removeControl('currentPassword');
+      this.form?.removeControl('password1');
+      this.form?.removeControl('password2');
+      this.form?.clearValidators();
+    }
+  }
+
+  private getSignUpForm(): FormGroup {
+    return this.fb.group({
+      username: this.usernameFormControl,
+      email: this.emailFormControl,
+      ageConditions: [, Validators.requiredTrue],
+      acceptedConditions: [, Validators.requiredTrue],
+      password1: this.password1FormControl,
+      password2: this.password2FormControl
+    }, {
+      validators: [PasswordMatchValidator.match('password1', 'password2')]
+    });
+  }
+
+  private getUpdateSettingsForm(): FormGroup {
+    return this.fb.group({
+      username: this.usernameFormControl,
+      email: this.emailFormControl,
+
+    }, {});
   }
 }
