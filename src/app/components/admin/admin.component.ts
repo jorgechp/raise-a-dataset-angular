80f07192a9225c90bf4dd40c81867aa204b016ba
jorgechp <jorge.chamorro@udl.cat>
2024-09-02 22:38:25 +0200

    Add lazy load for admin tabs

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -3,11 +3,12 @@ import {MatTabsModule} from "@angular/material/tabs";
 import {AdminUsersComponent} from "./admin-users/admin-users.component";
 import {AdminRepositoriesComponent} from "./admin-repositories/admin-repositories.component";
 import {AdminDatasetsComponent} from "./admin-datasets/admin-datasets.component";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-admin-tab',
   standalone: true,
-  imports: [MatTabsModule, AdminUsersComponent, AdminRepositoriesComponent, AdminDatasetsComponent],
+  imports: [MatTabsModule, AdminUsersComponent, AdminRepositoriesComponent, AdminDatasetsComponent, NgIf],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
