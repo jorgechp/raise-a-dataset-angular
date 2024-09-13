1d34d2b164945d54bf2c69ce996c6d11b38adc8a
jorchamo <jorge.chamorro@udl.cat>
2024-09-13 16:08:02 +0200

    Add FAIR principles

--- src/app/components/fair-principles/fair-principles.component.ts
+++ src/app/components/fair-principles/fair-principles.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatTabsModule} from "@angular/material/tabs";
 
 @Component({
   selector: 'app-fair-principles',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule],
   templateUrl: './fair-principles.component.html',
   styleUrl: './fair-principles.component.scss'
 })
